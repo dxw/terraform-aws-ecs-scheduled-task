15b00e0bc00140d0c02bcee9e7c92c0f344d8edf
Chris Wright <chris@dxw.com>
2018-11-14 15:57:07 +0000

    (Feature) Create scheduled task module
    * Creates the required roles for ECS and CloudWatch
    * Creates a task given container definitions
    * Runs on the given cron schedule

---
+++ README.md
@@ -0,0 +1,76 @@
+# Terraform AWS ECS Scheduled Task
+
+A Terraform module to create a scheduled task in AWS ECS
+
+## Usage
+
+``` hcl
+module "scheduled_task" {
+  source  = "github.com/dxw/terraform-aws-ecs-scheduled-task"
+  version = "0.1"
+
+  name                  = "my_awesome_task"
+  environment           = "staging"
+  container_definitions = "${file(./path/to/container-definitions.json)}"
+  schedule_expression   = "0 * * * *"
+  cluster_arn           = "my_awesome_cluster"
+}
+```
+
+## Configuration
+
+The following variables can be configured:
+
+### Required
+
+#### `name`
+
+- **Description**: Unique name for resources
+- **Default**: `none`
+
+#### `environment`
+
+- **Description**: Environment - appended to ${var.name} for resources
+- **Default**: `none`
+
+#### `container_definitions`
+
+- **Description**: Task container defintions
+- **Default**: `none`
+
+#### `schedule_expression`
+
+- **Description**: Schedule expression (cron) for when to run task
+- **Default**: `none`
+
+#### `cluster_arn`
+
+- **Description**: ARN of cluster on which to run task
+- **Default**: `none`
+
+### Optional
+
+#### `network_mode`
+
+- **Description**: Task network mode
+- **Default**: `bridge`
+
+### Outputs
+
+The following outputs are exported:
+
+#### `scheduled_task_ecs_execution_role_id`
+
+- **Description**: Scheduled Task ECS Role ID
+
+#### `scheduled_task_ecs_role_id`
+
+- **Description**: Scheduled Task ECS Role ID
+
+#### `scheduled_task_cloudwatch_role_id`
+
+- **Description**: Scheduled Task CloudWatch Role ID
+
+#### `scheduled_task_arn`
+
+- **Description**: Scheduled Task ARN
---
+++ main.tf
@@ -0,0 +1,75 @@
+## ECS task exectution role
+
+resource "aws_iam_role" "scheduled_task_ecs_execution" {
+  name               = "${var.name}-${var.environment}-scheduled-task-ecs-execution-role"
+  assume_role_policy = "${file("./policies/scheduled-task-ecs-assume-role-policy.json")}"
+}
+
+data "template_file" "scheduled_task_ecs_execution_policy" {
+  template = "${file("./policies/scheduled-task-ecs-execution-policy.json")}"
+}
+
+resource "aws_iam_role_policy" "scheduled_task_ecs_execution" {
+  name   = "${var.name}-${var.environment}-scheduled-task-ecs-execution-policy"
+  role   = "${aws_iam_role.scheduled_task_ecs_execution.id}"
+  policy = "${data.template_file.scheduled_task_ecs_execution_policy.rendered}"
+}
+
+## ECS task role
+
+resource "aws_iam_role" "scheduled_task_ecs" {
+  name               = "${var.name}-${var.environment}-scheduled-task-ecs-role"
+  assume_role_policy = "${file("./policies/scheduled-task-ecs-assume-role-policy.json")}"
+}
+
+## Cloudwatch event role
+
+resource "aws_iam_role" "scheduled_task_cloudwatch" {
+  name               = "${var.name}-${var.environment}-scheduled-task-cloudwatch-role"
+  assume_role_policy = "${file("./policies/scheduled-task-cloudwatch-assume-role-policy.json")}"
+}
+
+data "template_file" "scheduled_task_cloudwatch_policy" {
+  template = "${file("./policies/scheduled-task-cloudwatch-policy.json")}"
+
+  vars {
+    task_execution_role_arn = "${aws_iam_role.scheduled_task_ecs_execution.arn}"
+  }
+}
+
+resource "aws_iam_role_policy" "scheduled_task_cloudwatch_policy" {
+  name   = "${var.name}-${var.environment}-scheduled-task-cloudwatch-policy"
+  role   = "${aws_iam_role.scheduled_task_cloudwatch.id}"
+  policy = "${data.template_file.scheduled_task_cloudwatch_policy.rendered}"
+}
+
+## ECS task definition
+
+resource "aws_ecs_task_definition" "scheduled_task" {
+  family                   = "${var.name}-${var.environment}-scheduled-task"
+  container_definitions    = "${var.container_definitions}"
+  requires_compatibilities = ["EC2"]
+  network_mode             = "${var.network_mode}"
+  execution_role_arn       = "${aws_iam_role.scheduled_task_ecs_execution.arn}"
+  task_role_arn            = "${aws_iam_role.scheduled_task_ecs.arn}"
+}
+
+## Cloudwatch event
+
+resource "aws_cloudwatch_event_rule" "scheduled_task" {
+  name                = "${var.name}_${var.environment}_scheduled_task"
+  description         = "Run ${var.name}_${var.environment} task at a scheduled time (${var.schedule_expression})"
+  schedule_expression = "${var.schedule_expression}"
+}
+
+resource "aws_cloudwatch_event_target" "scheduled_task" {
+  target_id = "${var.name}_${var.environment}_scheduled_task_target"
+  rule      = "${aws_cloudwatch_event_rule.scheduled_task.name}"
+  arn       = "${var.cluster_arn}"
+  role_arn  = "${aws_iam_role.scheduled_task_cloudwatch.arn}"
+
+  ecs_target {
+    task_count          = "${var.task_count}"
+    task_definition_arn = "${aws_ecs_task_definition.scheduled_task.arn}"
+  }
+}
