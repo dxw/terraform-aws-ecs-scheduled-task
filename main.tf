04d255dcb019c2ad0bd59dcf22cd936efa0fbf9b
Chris Wright <chris@dxw.com>
2018-11-20 13:19:44 +0000

    (Chore) Shorten IAM role / policy names
    * IAM roles have a maximum of 64 characters.
    * `${var.name}` and `${var.environment}` are injected into the resource names.
    Shortening the rest of the name allows longer var.name and
    var.environment

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 ## ECS task exectution role
 
 resource "aws_iam_role" "scheduled_task_ecs_execution" {
-  name               = "${var.name}-${var.environment}-scheduled-task-ecs-execution-role"
+  name               = "${var.name}-${var.environment}-st-ecs-execution-role"
   assume_role_policy = "${file("${path.module}/policies/scheduled-task-ecs-execution-assume-role-policy.json")}"
 }
 
@@ -10,7 +10,7 @@ data "template_file" "scheduled_task_ecs_execution_policy" {
 }
 
 resource "aws_iam_role_policy" "scheduled_task_ecs_execution" {
-  name   = "${var.name}-${var.environment}-scheduled-task-ecs-execution-policy"
+  name   = "${var.name}-${var.environment}-st-ecs-execution-policy"
   role   = "${aws_iam_role.scheduled_task_ecs_execution.id}"
   policy = "${data.template_file.scheduled_task_ecs_execution_policy.rendered}"
 }
@@ -18,14 +18,14 @@ resource "aws_iam_role_policy" "scheduled_task_ecs_execution" {
 ## ECS task role
 
 resource "aws_iam_role" "scheduled_task_ecs" {
-  name               = "${var.name}-${var.environment}-scheduled-task-ecs-role"
+  name               = "${var.name}-${var.environment}-st-ecs-role"
   assume_role_policy = "${file("${path.module}/policies/scheduled-task-ecs-assume-role-policy.json")}"
 }
 
 ## Cloudwatch event role
 
 resource "aws_iam_role" "scheduled_task_cloudwatch" {
-  name               = "${var.name}-${var.environment}-scheduled-task-cloudwatch-role"
+  name               = "${var.name}-${var.environment}-st-cloudwatch-role"
   assume_role_policy = "${file("${path.module}/policies/scheduled-task-cloudwatch-assume-role-policy.json")}"
 }
 
@@ -38,7 +38,7 @@ data "template_file" "scheduled_task_cloudwatch_policy" {
 }
 
 resource "aws_iam_role_policy" "scheduled_task_cloudwatch_policy" {
-  name   = "${var.name}-${var.environment}-scheduled-task-cloudwatch-policy"
+  name   = "${var.name}-${var.environment}-st-cloudwatch-policy"
   role   = "${aws_iam_role.scheduled_task_cloudwatch.id}"
   policy = "${data.template_file.scheduled_task_cloudwatch_policy.rendered}"
 }
