89926538624dccede4362a65744d84fd856dbd54
Chris Wright <chris@dxw.com>
2020-04-28 12:49:10 +0100

    (Chore) Terraform v12 syntax
    * Updates the syntax to for v12 to prevent warnings

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "scheduled_task_ecs_execution_role_id" {
-  value = "${aws_iam_role.scheduled_task_ecs_execution.id}"
+  value = aws_iam_role.scheduled_task_ecs_execution.id
 }
 
 output "scheduled_task_ecs_role_id" {
-  value = "${aws_iam_role.scheduled_task_ecs.id}"
+  value = aws_iam_role.scheduled_task_ecs.id
 }
 
 output "scheduled_task_cloudwatch_role_id" {
-  value = "${aws_iam_role.scheduled_task_cloudwatch.id}"
+  value = aws_iam_role.scheduled_task_cloudwatch.id
 }
 
 output "scheduled_task_arn" {
-  value = "${aws_ecs_task_definition.scheduled_task.arn}"
+  value = aws_ecs_task_definition.scheduled_task.arn
 }
